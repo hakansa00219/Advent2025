e229f282e068722006597766279eca878b003240
Hakan Sarıoğlu <61359973+hakansa00219@users.noreply.github.com>
2025-12-01 19:18:51 +0300

    day1 part1 and part2
    part1 was easy with just modular but couldn't get the right part2 answer with that. refactor to looping.

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,4 +1,30 @@
 from input_loader import load_input
 
 text = load_input("input.txt")
-print(text)
+
+currentDial = 50
+part1 = 0
+part2 = 0
+splitText = text.split('\n')
+counter = 0
+for line in splitText:
+    counter += 1
+    rotation = line[0]
+    degrees = int(line[1:])
+    while degrees != 0:
+        currentDial += -1 if rotation == 'L' else 1
+        degrees -= 1
+        if currentDial == 100:
+            currentDial = 0
+        elif currentDial == -1:
+            currentDial = 99
+
+        if currentDial == 0 and degrees != 0:
+            part2 += 1
+
+    if currentDial == 0:
+        part1 += 1
+
+part2 += part1
+print("Part1:" + str(part1))
+print("Part2:" + str(part2))
