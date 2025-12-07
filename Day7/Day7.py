44743d7c0ac365d71c61d705ac8e929d572d5afe
Hakan Sarıoğlu <61359973+hakansa00219@users.noreply.github.com>
2025-12-07 17:33:40 +0300

    day7 part2 dfs with cache cause you need it

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -1,3 +1,4 @@
+from splitter import Splitter
 from input_loader import load_input
 
 text = load_input("input.txt")
@@ -33,7 +34,59 @@ while len(stack) > 0:
     if y == len(lines) - 1:
         stack.pop()
 
-
-
 print("Part1:" + str(part1))
-print("Part2:" + str(part2))
+beamStack = [("DO", 0, startIndex)]
+cache = {}
+
+while beamStack:
+    process, y, x, *rest = beamStack.pop()
+
+    if process == "DO":
+
+        if y == len(lines) - 1:
+            cache[(y, x)] = 1
+            part2 += 1
+            continue
+
+        c = lines[y][x]
+
+        if c == '^':
+            left = (y, x - 1)
+            right = (y, x + 1)
+
+            # cache skip
+            if (y, x) in cache:
+                val = cache.get(left, 0) + cache.get(right, 0)
+                continue
+
+            #parent merge
+            beamStack.append(("MERGE_SPLIT", y, x, left, right))
+
+            #children
+            beamStack.append(("DO", right[0], right[1]))
+            beamStack.append(("DO", left[0], left[1]))
+
+        else:
+            down = (y + 1, x)
+
+            #cache skip
+            if (y, x) in cache:
+                val = cache.get(down, 0)
+
+            #down
+            beamStack.append(("MERGE_DOWN", y, x, down))
+            beamStack.append(("DO", down[0], down[1]))
+
+    elif process == "MERGE_SPLIT":
+        left,right = rest
+        val = cache.get(left, 0) + cache.get(right, 0)
+        cache[(y,x)] = val
+
+    elif process == "MERGE_DOWN":
+        (down, ) = rest
+        val = cache.get(down, 0)
+        cache[(y,x)] = val
+
+
+print("Part2:" + str(cache.get((0, startIndex))))
+
---
+++ Day7/splitter.py
@@ -0,0 +1,8 @@
+class Splitter:
+    def __init__(self, merge, ypos, xpos, left, right):
+        self.merge = merge
+        self.y = ypos
+        self.x = xpos
+        self.left = left
+        self.right = right
+        self.value = 0
